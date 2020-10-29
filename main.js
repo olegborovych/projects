572c095b5ea1bc13ed4d5f9d5ddca2d1bc124b26
olegborovych <olborovych@gmail.com>
2020-10-29 10:01:21 +0200

    change_v1

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-'use strict';
+const uah = 1;
+const uahsell = 1.01;
+const usd = 27.8;
+const euro = 33.6;
+const zloty = 7;
+
+let usdBuy = document.getElementById('usd-buy');
+let usdSell = document.getElementById('usd-sell');
+
+let euroBuy = document.getElementById('euro-buy');
+let euroSell = document.getElementById('euro-sell');
+
+let zlotyBuy = document.getElementById('zloty-buy');
+let zlotySell = document.getElementById('zloty-sell');
+
+usdBuy.innerText = (uah * usd)
+euroBuy.innerText = (uah * euro)
+zlotyBuy.innerText = (uah * zloty)
+
+usdSell.innerText = (uahsell * usd)
+euroSell.innerText = (uahsell * euro)
+zlotySell.innerText = (uahsell * zloty)
